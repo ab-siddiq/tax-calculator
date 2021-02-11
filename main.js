8b54af1ffc0a14c0f708ec1d46cb98b5f658cc59
siddiq <siddiq.aiub@gmail.com>
2021-02-11 23:42:52 +0600

    make change

--- main.js
+++ main.js
@@ -1,35 +1,28 @@
 
-document.getElementById("calculate").addEventListener("click", function () {
+function taxCalculation() {
     // const nameInput = getInputNumber("tax-name-input");
     // console.log(nameInput);
     const invest = getInputNumber("tax-actual-invest");
-    console.log(invest);
     const income = getInputNumber("tax-total-income");
-    console.log(income);
     const taxableIncome = getInputNumber("tax-annual-taxable-income");
-    console.log(taxableIncome);
     const allowableInvestment = getInputNumber("tax-maximum-allowable-investment");
     // console.log(allowableInvestment);
 
     const minValue = Math.min(invest, taxableIncome * .25, 1500000);
-    console.log(minValue);
 
     document.getElementById("tax-maximum-allowable-investment").value = minValue;
-
-    // const outputActualInvestUnitFund = getInputNumber("output-actual-invest-unit-fund");
-    // const outputActualInvestDPS = getInputNumber("output-actual-invest-dps");
-    // const outputMaximumInvestUnitFund = getInputNumber("output-maximum-investment-unit-fund");
-    // const outputMaximumInvestDPS = getInputNumber("output-maximum-investment-dps");
-    // const outputSaveTaxUnitFund = getInputNumber("output-save-tax-unit-fund");
-    // const outputSaveTaxDPS = getInputNumber("output-save-tax-dps");
-    // const outputReturnTaxUnitFund = getInputNumber("output-return-tax-unit-fund");
-    // const outputReturnTaxDPS = getInputNumber("output-return-tax-dps");
-
     document.getElementById("output-actual-invest-unit-fund").value = invest;
 
+    const outputSaveTaxUnitFund = getInputNumber("output-save-tax-unit-fund");
+    console.log(outputSaveTaxUnitFund,"kasdf");
+    const outputSaveTaxDPS = getInputNumber("output-save-tax-dps");
+
     const outputActualInvestUnitFund = getInputNumber("output-actual-invest-unit-fund");
+    const outputActualInvestDPS = getInputNumber("output-actual-invest-dps");
+
     document.getElementById("output-actual-invest-dps").value = outputActualInvestUnitFund;
     document.getElementById("output-maximum-investment-unit-fund").value = minValue;
+
     let saveTaxUnit = 0;
     let saveTaxDPS = 0;
     const outputMaximumInvestUnitFund = getInputNumber("output-maximum-investment-unit-fund");
@@ -42,14 +35,33 @@ document.getElementById("calculate").addEventListener("click", function () {
         document.getElementById("output-save-tax-unit-fund").value = saveTax;
         document.getElementById("output-save-tax-dps").value = saveTaxDPS;
     } else {
-        saveTax = outputMaximumInvestUnitFund * .15;
+        saveTaxUnit = outputMaximumInvestUnitFund * .15;
         saveTaxDPS = outputMaximumInvestDPS * .15;
-        console.log("else", saveTax);
-        document.getElementById("output-save-tax-unit-fund").value = saveTax;
+        console.log("else", saveTaxUnit);
+        document.getElementById("output-save-tax-unit-fund").value = saveTaxUnit;
         document.getElementById("output-save-tax-dps").value = saveTaxDPS;
     }
 
-})
+    // const outputReturnTaxUnitFund = Math.ceil((saveTaxUnit/outputActualInvestUnitFund)*10);
+    // const outputReturnTaxDPS = Math.ceil((saveTaxDPS / outputActualInvestUnitFund)*10);
+    const divideTaxUnit = (saveTaxUnit / outputActualInvestUnitFund);
+    const divideTaxDPS = (saveTaxDPS / outputActualInvestUnitFund);
+    const multiplyTaxUnit = divideTaxUnit * 100;
+    const multiplyTaxDPS = divideTaxDPS * 100;
+    const calculateTaxUnit = Math.ceil(multiplyTaxUnit);
+    const calculateTaxDPS = Math.ceil(multiplyTaxDPS);
+    // console.log(Math.ceil(a*10), Math.ceil(b*10));
+    console.log(calculateTaxUnit, calculateTaxDPS);
+    document.getElementById("output-return-tax-unit-fund").value =calculateTaxUnit+"%";
+    document.getElementById("output-return-tax-dps").value =calculateTaxDPS+"%";
+
+    document.getElementById("max-allowable-investment").innerHTML = outputMaximumInvestUnitFund; 
+    document.getElementById("ekus-unit-fund").innerHTML = saveTaxUnit; 
+    document.getElementById("implicitly-earn").innerHTML = calculateTaxUnit+"%"; 
+    
+
+
+}
 
 // function calculate(params) {
     
@@ -57,6 +69,12 @@ document.getElementById("calculate").addEventListener("click", function () {
 function getInputNumber(id) {
     const inputNumber = document.getElementById(id).value;
     const parseInputNumber = parseFloat(inputNumber);
-    // console.log(typeof( parseInputNumber));
     return parseInputNumber;
+}
+
+function taxReset() {
+    document.getElementById("tax-name-input").value = ""; 
+    document.getElementById("tax-actual-invest").value = ""; 
+    document.getElementById("tax-total-income").value = ""; 
+    document.getElementById("tax-annual-taxable-income").value = ""; 
 }
